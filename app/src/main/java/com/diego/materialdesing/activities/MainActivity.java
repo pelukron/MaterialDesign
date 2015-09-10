c4e58230802b896d96ff60530b7180ca2896722e
Diego Moreno <diegohmore@hotmail.com>
2015-09-10 12:08:23 -0500

    mejor estructura de archivos

--- app/src/main/java/com/diego/materialdesing/activities/MainActivity.java
+++ app/src/main/java/com/diego/materialdesing/activities/MainActivity.java
@@ -1,9 +1,11 @@
-package com.diego.materialdesing;
+package com.diego.materialdesing.activities;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+
+import com.diego.materialdesing.R;
 
 public class MainActivity extends AppCompatActivity {
 
