d7047793646f0aae42a4eb3d93f42d82dc0b55ca
Diego Moreno <diegohmore@hotmail.com>
2015-09-11 22:53:26 -0500

    boton flotante, asi como implementacion de snackbar

--- app/src/main/java/com/diego/materialdesing/activities/MainActivity.java
+++ app/src/main/java/com/diego/materialdesing/activities/MainActivity.java
@@ -1,18 +1,43 @@
 package com.diego.materialdesing.activities;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.FrameLayout;
 
 import com.diego.materialdesing.R;
 
 public class MainActivity extends AppCompatActivity {
 
+    FloatingActionButton fabBtn;
+    FrameLayout rootLayout;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        initInstance();
+    }
+
+    private void initInstance() {
+        rootLayout = (FrameLayout) findViewById(R.id.rootLayout);
+        fabBtn = (FloatingActionButton)findViewById(R.id.fabBtn);
+        fabBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Snackbar.make(rootLayout, "Snackbar", Snackbar.LENGTH_SHORT)
+                        .setAction("Undo", new View.OnClickListener() {
+                            @Override
+                            public void onClick(View v) {
+
+                            }
+                        }).show();
+            }
+        });
     }
 
     @Override
