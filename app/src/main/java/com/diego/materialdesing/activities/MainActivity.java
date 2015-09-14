141606a12156e42c3559043b7d028a788827b6f8
Diego Moreno <diegohmore@hotmail.com>
2015-09-14 11:34:39 -0500

    configuracion del toolbar con icono agregado

--- app/src/main/java/com/diego/materialdesing/activities/MainActivity.java
+++ app/src/main/java/com/diego/materialdesing/activities/MainActivity.java
@@ -1,44 +1,27 @@
 package com.diego.materialdesing.activities;
 
 import android.os.Bundle;
-import android.support.design.widget.CoordinatorLayout;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
-import android.widget.FrameLayout;
 
 import com.diego.materialdesing.R;
 
 public class MainActivity extends AppCompatActivity {
 
-    FloatingActionButton fabBtn;
-    CoordinatorLayout rootLayout;
+    Toolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        initInstance();
+        setupToolbar();
     }
 
-    private void initInstance() {
-        rootLayout = (CoordinatorLayout) findViewById(R.id.rootLayout);
-        fabBtn = (FloatingActionButton)findViewById(R.id.fabBtn);
-        fabBtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Snackbar.make(rootLayout, "Snackbar", Snackbar.LENGTH_SHORT)
-                        .setAction("Undo", new View.OnClickListener() {
-                            @Override
-                            public void onClick(View v) {
-
-                            }
-                        }).show();
-            }
-        });
+    private void setupToolbar() {
+        toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
     }
 
     @Override
